40a3b6f5b7dea70176802b19689065e7a07600e7
yong yang <y9415019@hotmail.com>
2018-09-17 22:23:47 -0700

    add login.

---
+++ src/main/java/com/yy/EmployeeManagement/Controller/EmployeeServlet.java
@@ -0,0 +1,67 @@
+package com.yy.EmployeeManagement.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yy.EmployeeManagement.Service.EmployeeService;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EmployeeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String method = request.getParameter("method");
+		if (method != null) {
+			if (method.equals("login")) {
+				this.login(request, response);
+			}
+		}
+	}
+
+	private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmployeeService service = new EmployeeService();
+		String userName = request.getParameter("username");
+		String passWord = request.getParameter("password");
+
+		String role = service.LoginAsRole(userName, passWord);
+		if (role.equals("role_user")) {
+			request.setAttribute("message", "login as user.");
+		}
+		if (role.equals("role_admin")) {
+			request.setAttribute("message", "login as admin");
+		}
+		if (role.equals("not_allowed")) {
+			request.setAttribute("message", "not allowed!");
+		}
+		request.getRequestDispatcher("/message.jsp").forward(request, response);
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,27 @@
 <html>
 <body>
-    <h2>Jersey RESTful Web Application!</h2>
-    <p><a href="webapi/myresource">Jersey resource</a>
-    <p>Visit <a href="http://jersey.java.net">Project Jersey website</a>
-    for more information on Jersey!
+   <form action="/EmployeeManagement/EmployeeServlet?method=login" method="post">
+     <table boarder="1" align="center">
+         <caption>User Login</caption>
+         <tr>
+             <th>UserName</th>
+             <td>
+                 <input type="text" name="username"/>
+             </td>
+         </tr>
+         <tr>
+             <th>Password</th>
+             <td>
+                 <input type="password" name="password"/>
+             </td>
+         </tr>
+         <tr>
+             <td>
+                 <input type="submit" value="Submit"/>
+             </td>
+         </tr>
+     </table>
+   
+   </form>
 </body>
 </html>
